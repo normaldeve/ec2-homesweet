aa39b3b5d3cf38e992e755f7f292d67e7e7fd7e1
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-04 17:43:18 +0900

    Feat: 로그는 에러만!

--- k6/scripts/product-search-keyword.js
+++ k6/scripts/product-search-keyword.js
@@ -3,19 +3,18 @@ import { check } from "k6";
 import { sleep } from "k6";
 
 
-const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMSIsImVtYWlsIjoianVubnVraW0xMDA3QGdtYWlsLmNvbSIsIm5hbWUiOiLquYDspIDsmrAiLCJwcm92aWRlciI6Imdvb2dsZSIsInJvbGUiOiJTRUxMRVIiLCJpYXQiOjE3NjQxNTE2NjgsImV4cCI6MTc2NDE2OTY2OH0.oiXnHA9BlYKgXLuMJKnEeINcFMFRIVWYjjEvW9ZyncZd8JifGA0i0C686wYtvjmnAFPf81i2hnbO2LwtjOR6ZQ"
-
+const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMSIsImVtYWlsIjoianVubnVraW0xMDA3QGdtYWlsLmNvbSIsIm5hbWUiOiLquYDspIDsmrAiLCJwcm92aWRlciI6Imdvb2dsZSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzY0ODMxNjIwLCJleHAiOjE3NjQ4NDk2MjB9.xqQ-U4uYUvPjK3nRwJERjJLrWurDH51_9pxnM9Bj6uhh94GSn0rljReg7YxWGr0FXZDEhs6OdIlMjsnzHhHQrw"
 export const options = {
     stages: [
-        {duration: '3m', target: 100}
+        {duration: '1m', target: 100}
     ]
 };
 
 export default function () {
-    const keyword = encodeURIComponent("침대프레임");
+    const keyword = encodeURIComponent("엘린하우스 침대");
 
     const response = http.get(
-        `http://localhost:8080/api/v1/products/search?size=12&keyword=${keyword}&sortType=RECOMMENDED`,
+        `http://3.35.228.233:8080/api/v1/search/products?size=12&keyword=${keyword}&sortType=RECOMMENDED`,
         {
             headers: {
                 "Authorization": `Bearer ${token}`
