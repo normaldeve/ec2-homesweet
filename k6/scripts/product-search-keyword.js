9efa5421de351806904eeb66d08ef3eddb561ed0
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-04 18:47:26 +0900

    feat: 엘라스틱 모니터링

--- k6/scripts/product-search-keyword.js
+++ k6/scripts/product-search-keyword.js
@@ -5,13 +5,21 @@ import { sleep } from "k6";
 
 const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMSIsImVtYWlsIjoianVubnVraW0xMDA3QGdtYWlsLmNvbSIsIm5hbWUiOiLquYDspIDsmrAiLCJwcm92aWRlciI6Imdvb2dsZSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzY0ODMxNjIwLCJleHAiOjE3NjQ4NDk2MjB9.xqQ-U4uYUvPjK3nRwJERjJLrWurDH51_9pxnM9Bj6uhh94GSn0rljReg7YxWGr0FXZDEhs6OdIlMjsnzHhHQrw"
 export const options = {
-    stages: [
-        {duration: '1m', target: 100}
-    ]
+    scenarios: {
+        search_rps_test: {
+            executor: "constant-arrival-rate",
+            rate: 2000,
+            timeUnit: "1s",
+            duration: "3m",
+            preAllocatedVUs: 200,
+            maxVUs: 5000,
+        }
+    }
 };
 
+
 export default function () {
-    const keyword = encodeURIComponent("엘린하우스 침대");
+    const keyword = encodeURIComponent("엘린하우스");
 
     const response = http.get(
         `http://3.35.228.233:8080/api/v1/search/products?size=12&keyword=${keyword}&sortType=RECOMMENDED`,
@@ -25,6 +33,4 @@ export default function () {
     check(response, {
         "is status 200": (r) => r.status === 200,
     });
-
-    sleep(1);
 }
