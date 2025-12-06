69cb8ec5dc823750e7e21b3cedb22cb019bb395b
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-06 15:29:50 +0900

    주소 수정

--- k6/search/utils/http_client.js
+++ k6/search/utils/http_client.js
@@ -4,7 +4,7 @@ const API_HOST = "http://43.203.103.201:8080";
 const SEARCH_URL = `${API_HOST}/api/v1/search/products`;
 const CATEGORY_URL = `${API_HOST}/api/v1/categories`;
 
-const TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDAwMSIsImVtYWlsIjoianVubnVraW0xMDA3QGdtYWlsLmNvbSIsIm5hbWUiOiLquYDspIDsmrAiLCJwcm92aWRlciI6Imdvb2dsZSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzY0OTA2MjEyLCJleHAiOjE3NjQ5MjQyMTJ9.6IEWY58Uz5xlJVu8OuFzsLiY2AiwDyAgwGnDyvzneea6-HhD8wIbXB4o9WMCEjcn3ms4JRWzd9bud0CMbM52Og";
+const TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDAwMSIsImVtYWlsIjoianVubnVraW0xMDA3QGdtYWlsLmNvbSIsIm5hbWUiOiLquYDspIDsmrAiLCJwcm92aWRlciI6Imdvb2dsZSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzY0OTI2NjI0LCJleHAiOjE3NjQ5NDQ2MjR9.S9zSGLKubYooY_dX74VR5oJb2auDVGktfTHvHQNwOk8Fmk36A4gx41wJBeqQbhyJI13wOc33TjC85EhTl9Q_Mw"
 
 export function getTopCategories() {
     return http.get(`${CATEGORY_URL}/top`);
